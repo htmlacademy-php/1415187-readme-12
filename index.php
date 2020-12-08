b01894ade651fc1e118e280d9b7f5d34a8f95684
M2rk13 <70945005+M2rk13@users.noreply.github.com>
2020-12-08 11:06:21 +0300

    модуль1-исправление
    Заменено сравнение на строгое сравнение

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ $user_name = 'Mark'; // укажите здесь ваше имя
             </div>
         </form>
         <div class="header__nav-wrapper">
-          <?php if ($is_auth == 1) : ?>
+          <?php if ($is_auth === 1) : ?>
             <nav class="header__nav">
                 <ul class="header__my-nav">
                     <li class="header__my-page header__my-page--popular">
