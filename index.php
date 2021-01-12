a970424ceb366b600e2661bc84c078f565f3ab42
M2rk13 <70945005+M2rk13@users.noreply.github.com>
2021-01-12 12:17:25 +0300

    Update index.php

--- index.php
+++ index.php
@@ -254,7 +254,7 @@ $popular_posts = [
                                 <a class="post-link__external" href="http://<?=$post['content']?>" title="Перейти по ссылке">
                                     <div class="post-link__info-wrapper">
                                         <div class="post-link__icon-wrapper">
-                                            <img src="https://www.google.com/s2/favicons?domain=<?=post['content']?>" alt="Иконка">
+                                            <img src="https://www.google.com/s2/favicons?domain=<?=$post['content']?>" alt="Иконка">
                                         </div>
                                         <div class="post-link__info">
                                             <h3><?=$post['title']?></h3>
